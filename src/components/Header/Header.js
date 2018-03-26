348cf3ad7130dfb699cdb3adb3fcb7b4646400db
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-03-25 22:12:09 -0600

    Add router
    Install dev with the command npm install react-router-dom --save

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import './Header.css';
-
+import { Link } from 'react-router-dom';
 class Header extends Component {
     render() {
         return (
             <div className="navbar has-shadow">
                 <div className="container">
                     <div className="navbar-brand">
-                        <a href="/" className="navbar-item">MyCompany</a>
+                        <Link to="/" className="navbar-item">MyCompany</Link>
 
                         <div className="navbar-burger">
                             <span></span>
@@ -17,10 +17,10 @@ class Header extends Component {
                     </div>
                     <div className="navbar-menu">
                         <div className="navbar-end ">
-                            <a href="/" className="navbar-item r-item">Home</a>
-                            <a className="navbar-item r-item">About</a>
-                            <a className="navbar-item r-item">Features</a>
-                            <a href="/faq" className="navbar-item r-item">FAQ</a>
+                            <Link  to="/" className="navbar-item r-item">Home</Link>
+                            <Link to="#" className="navbar-item r-item">About</Link>
+                            <Link to="#" className="navbar-item r-item">Features</Link>
+                            <Link to="/faq" className="navbar-item r-item">FAQ</Link>
                             <div className="navbar-item">
                                 <p className="control">
                                     <a className="button is-primary is-outlined">
