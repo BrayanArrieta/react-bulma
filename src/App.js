1cd7a736cff77014eb9344169a45e1896a3a99d4
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-03-25 22:17:45 -0600

    Change view between different components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import Faq from './components/Faq/Faq'
+import {Route} from 'react-router-dom'
 class App extends Component {
   render() {
     return (
       <div className="App">
             <Header></Header>
+                <Route exact={true} path="/" component={Home} > </Route>
+                <Route path="/faq" component={Faq} > </Route>
             <Footer></Footer>
       </div>
     );
---
+++ src/components/Faq/Faq.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './Footer.css';
+
+class Faq extends Component {
+    render() {
+        return (
+            <div className="App">
+                Faq
+            </div>
+        );
+    }
+}
+
+export default Faq;
