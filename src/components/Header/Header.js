c3055775feb30e03e64d21334c8b5bb706921cf8
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-03-25 22:01:11 -0600

    Add styles to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Header.css';
 
 class Header extends Component {
     render() {
         return (
-            <div className="App">
-                Header
+            <div className="navbar has-shadow">
+                <div className="container">
+                    <div className="navbar-brand">
+                        <a href="/" className="navbar-item">MyCompany</a>
+
+                        <div className="navbar-burger">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="navbar-menu">
+                        <div className="navbar-end ">
+                            <a href="/" className="navbar-item r-item">Home</a>
+                            <a className="navbar-item r-item">About</a>
+                            <a className="navbar-item r-item">Features</a>
+                            <a href="/faq" className="navbar-item r-item">FAQ</a>
+                            <div className="navbar-item">
+                                <p className="control">
+                                    <a className="button is-primary is-outlined">
+                                        <span className="icon">
+                                          <i className="fa fa-download"></i>
+                                        </span>
+                                        <span>Join Now</span>
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         );
     }
